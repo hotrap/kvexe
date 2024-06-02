e5dec28631fdc8a3f677427d67c2494acb7eb365
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-06-02 23:44:47 +0800

    Value prefix: load- and run-

--- src/main.cpp
+++ src/main.cpp
@@ -452,7 +452,6 @@ int main(int argc, char **argv) {
     }
 
     /* Operation counts*/
-    log << "operation counts: " << tester.GetOpParseCounts() << "\n";
     log << "notfound counts: " << tester.GetNotFoundCounts() << "\n";
     log << "stat end===" << std::endl;
   };
