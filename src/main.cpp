33b5aaa67eacb824ab5ffd7d4c92530f97c5d4e5
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-09-14 08:47:53 +0000

    Options for RALT

--- src/main.cpp
+++ src/main.cpp
@@ -1072,10 +1072,9 @@ int main(int argc, char **argv) {
                      po::value<int>(&compaction_pri)->required(),
                      "Method to pick SST to compact (rocksdb::CompactionPri)");
 
+  // Options for RALT
   desc.add_options()("enable_auto_tuning", "enable auto-tuning");
-
   desc.add_options()("enable_sampling", "enable_sampling");
-
   desc.add_options()("ralt_bloom_bpk", po::value<int>(&ralt_bloom_bpk)->default_value(10), "The number of bits per key in RALT bloom filter.");
 
   po::variables_map vm;
