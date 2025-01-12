7b3ebccba3c495c6a46e926213b858276fabc8ce
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-01-12 14:10:41 +0000

    Enable hotrap after hot set size > 0

--- src/main.cpp
+++ src/main.cpp
@@ -1838,6 +1838,7 @@ int main(int argc, char **argv) {
         first_level_in_last_tier - 1, hot_set_size_limit, max_ralt_size,
         switches, hot_set_size_limit, fd_size);
     options.ralt = ralt;
+    options.disable_hotrap = true;
     ralt_size = ralt->GetRealPhySize();
   } else {
     std::cerr << "RALT disabled" << std::endl;
